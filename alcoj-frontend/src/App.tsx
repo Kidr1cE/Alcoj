70a55b80ed8d2eed877551b8e88e6b11eaf4cbe3
Kidr1cE <kang89963@gmail.com>
2024-04-04 21:28:00 +0800

    feat:add antd-frontend

---
+++ alcoj-frontend/src/App.tsx
@@ -0,0 +1,238 @@
+/* eslint-disable react/no-array-index-key */
+import React from 'react';
+import { Layout, Input, Select, Button, Card } from 'antd';
+import { CaretRightOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+import axios from 'axios'; // 假设您已经安装了axios库来处理HTTP请求
+
+const { Header, Footer, Sider, Content } = Layout;
+
+const backgroundColor = '#F0F0F0';
+
+const headerStyle: React.CSSProperties = {
+  textAlign: 'left',
+  color: '#000',
+  height: 64,
+  paddingInline: 48,
+  lineHeight: '64px',
+  backgroundColor: backgroundColor,
+  fontSize: 24,
+};
+
+const siderStyle: React.CSSProperties = {
+  textAlign: 'center',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: backgroundColor,
+  borderRadius: 10,
+  display: 'flex',
+  flexDirection: 'column',
+};
+
+const footerStyle: React.CSSProperties = {
+  display: 'flex',
+  color: '#fff',
+  backgroundColor: backgroundColor,
+  height: '25vh',
+  padding: '0',
+};
+
+const layoutStyle = {
+  borderRadius: 8,
+  overflow: 'hidden',
+  width: '100vw',
+  height: '100vh',
+};
+
+const codeSpaceStyle: React.CSSProperties = {
+  width: '100%',
+  height: '100%',
+  backgroundColor: '#FFF',
+  padding: '10px',
+  resize: 'none',
+  border: 'none',
+  boxShadow: 'none',
+
+  lineHeight: 1.5,
+  fontSize: 16,
+};
+
+const contentStyle: React.CSSProperties = {
+  display: 'flex',
+  textAlign: 'left',
+  minHeight: 120,
+  lineHeight: '120px',
+  borderRadius: 10,
+  backgroundColor: backgroundColor,
+  padding: '10px 5px 10px 10px',
+};
+
+const handleChange = (value: string) => {
+  console.log(`selected ${value}`);
+};
+
+const leftStyle: React.CSSProperties = {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '5px',
+  textAlign: 'left',
+  borderRadius: 10,
+  width: '100%',
+  position: 'relative',
+  backgroundColor: '#FFF',
+};
+
+const runButtonStyle: React.CSSProperties = {
+  width: '50px',
+  height: '50px',
+  borderRadius: 25,
+  textAlign: 'center',
+  position: 'absolute',
+  right: 10,
+  bottom: 10,
+};
+
+const rightStyle: React.CSSProperties = {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '10px',
+  textAlign: 'left',
+  borderRadius: 10,
+  width: '100%',
+  height: '100%',
+  alignItems: 'center',
+  padding: '10px',
+};
+
+const bottomStyle: React.CSSProperties = {
+  display: 'flex',
+  flexDirection: 'row',
+  height: '100%',
+  width: '100%',
+  padding: '0 10px',
+  gap: '10px',
+  backgroundColor: backgroundColor,
+}
+
+interface RunResponse {
+  output: string;
+  static_analysis: StaticAnalysis[];
+  runtime_analysis: Record<string, string>;
+}
+
+interface StaticAnalysis {
+  row: string;
+  column: string;
+  message: string;
+}
+
+const App: React.FC = () => {
+  const [selectedLanguage, setSelectedLanguage] = useState<string>('python3');
+  const [code, setCode] = useState<string>('');
+  const [input, setInput] = useState<string>('');
+
+  const [response, setResponse] = useState<RunResponse | null>(null);
+  let formattedOutput = response?.output?.replace(/\r?\n/g, '<br>');
+
+  const handleChange = (value: string) => {
+    setSelectedLanguage(value);
+  };
+
+  const handleCodeChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setCode(event.target.value);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setInput(event.target.value);
+  };
+
+  const handleRunButtonClick = async () => {
+    const requestBody = {
+      language: selectedLanguage,
+      code: code,
+      input: input,
+    };
+    console.log('Request body:', requestBody);
+
+    try {
+      const response = await axios.post('http://localhost:8080/alcoj/api/v1', requestBody);
+
+      if (response.status === 200 && response.data) {
+        setResponse(response.data);
+        console.log('Response:', response.data);
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  return (
+    <Layout style={layoutStyle}>
+      <Header style={headerStyle}>
+        AlcOJ
+      </Header>
+      <Layout style={{ color: "#FFF", height: '60vh' }}>
+        <Content style={contentStyle}>
+          <div style={leftStyle}>
+            <Select
+              defaultValue="python3"
+              style={{ width: 120 }}
+              onChange={handleChange}
+              options={[
+                { value: 'python3', label: 'Python3' },
+                { value: 'golang', label: 'Golang' },
+              ]}
+            />
+            <Input.TextArea
+              style={{ ...codeSpaceStyle, }}
+              placeholder="Code Space"
+              onChange={handleCodeChange}
+            />
+            <Button type="primary"
+              style={runButtonStyle}
+              icon={<CaretRightOutlined />}
+              onClick={handleRunButtonClick}
+            />
+          </div>
+        </Content>
+        <Sider width="25%" style={siderStyle}>
+          <div style={rightStyle}>
+            <Card style={{ width: "100%", height: "50%", overflowY: 'auto', }} title='Static Analysis'>
+              {response &&
+                response.static_analysis &&
+                response.static_analysis.map((analysis, index) => (
+                  <div key={index}>
+                    <p>
+                      {analysis.row}:{analysis.column} {analysis.message}
+                    </p>
+                  </div>
+                ))}
+            </Card>
+            <Card style={{ width: "100%", height: "50%", overflowY: 'auto', }} title='Runtime Analysis'>
+              {response && Object.keys(response.runtime_analysis).map((key, index) => (
+                <div key={index}>
+                  <p>{key}: {response.runtime_analysis[key]}</p>
+                </div>
+              ))}
+            </Card>
+          </div>
+        </Sider>
+      </Layout>
+
+      <Footer style={footerStyle}>
+        <div style={bottomStyle}>
+          <Input.TextArea
+            style={{ ...codeSpaceStyle, flex: 1 }}
+            placeholder="Input Space"
+            onChange={handleInputChange}
+          />
+          <Card style={{ flex: 1, overflowY: 'auto', }} title="Outputs">
+            {formattedOutput ? <div dangerouslySetInnerHTML={{ __html: formattedOutput }} /> : null}
+          </Card>
+        </div>
+      </Footer>
+    </Layout>
+  );
+};
+
+export default App;
---
+++ alcoj-frontend/src/Backend.tsx
@@ -0,0 +1,201 @@
+/* eslint-disable react/no-array-index-key */
+import React, { useState, useRef, useLayoutEffect, useEffect } from 'react';
+import { Layout, Select, Button, Card, Typography, Flex, Col, Row, Statistic } from 'antd';
+import CountUp from 'react-countup';
+import axios from 'axios'; // 假设您已经安装了axios库来处理HTTP请求
+
+const { Header, Content } = Layout;
+const { Paragraph, Text } = Typography;
+
+const backgroundColor = '#F0F0F0';
+
+type Worker = {
+    worker_id: string;
+    worker_status: string;
+}
+
+type Request = {
+    key: string;
+}
+
+type Response = {
+    worker_num: number;
+    queue_tasks: number;
+    finished_tasks: number;
+    workers: Worker[];
+}
+
+const WorkerCard: React.FC<Worker> = ({ worker_id, worker_status }) => {
+    const truncatedWorkerId = worker_id.slice(0, 8);
+
+    const getStatusColor = (status: string) => {
+        switch (status) {
+            case 'ready':
+                return '#73d13d';
+            case 'running':
+                return '#ffc53d';
+            case 'error':
+                return '#ff4d4f';
+            default:
+                return '#4096ff';
+        }
+    };
+
+    return (
+        <Card bordered={false} style={{
+            paddingBottom: '1px',
+            textAlign: 'center',
+            margin: '5px',
+        }}>
+            <div style={{
+                width: '100px',
+                height: '100px',
+                backgroundColor: getStatusColor(worker_status),
+                borderRadius: '8px',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}>
+                {worker_status}
+            </div>
+            <Paragraph copyable={{ text: worker_id }}>{truncatedWorkerId}</Paragraph>
+        </Card>
+    );
+};
+
+const WorkersList: React.FC<{ workers: Worker[] }> = ({ workers }) => (
+    <Flex>
+        {workers.map((worker) => (
+            <WorkerCard key={worker.worker_id} {...worker} />
+        ))}
+    </Flex>
+);
+
+
+const ResponseDisplay: React.FC<Response> = ({ worker_num, queue_tasks, finished_tasks, workers }) => {
+    const formatter = (value: any) => <CountUp
+        start={0}
+        end={value}
+        useEasing={false}
+        separator=","
+    />;
+    return (
+        <div style={{
+            backgroundColor: '#fff',
+            margin: '10px',
+            borderRadius: '8px',
+            padding: '10px',
+            height: 'calc(100vh - 120px)',
+        }}>
+            <Row gutter={21}>
+                <Col span={7}>
+                    <Statistic title="Online Workers" value={worker_num} formatter={formatter} />
+                </Col>
+                <Col span={7}>
+                    <Statistic title="Queue Tasks" value={queue_tasks} />
+                </Col>
+                <Col span={7}>
+                    <Statistic title="Finished Tasks" value={finished_tasks} />
+                </Col>
+            </Row>
+            <WorkersList workers={workers} />
+        </div>
+    );
+};
+const initialParsedResponse: Response = {
+    worker_num: 6,
+    queue_tasks: 20,
+    finished_tasks: 114514,
+    workers: [
+        { worker_id: 'acf9b8ba-f014-11ee-a043-00155da91e79', worker_status: 'Running' },
+        { worker_id: '9046dff8-f015-11ee-a604-00155da91e79', worker_status: 'Ready' },
+    ],
+};
+
+const Backend: React.FC = () => {
+    const wsRef = useRef<WebSocket | null>(null);
+    const [selectedLanguage, setSelectedLanguage] = useState<string>('python3');
+    const [message, setMessage] = useState('');
+    const [parsedResponse, setParsedResponse] = useState<Response>(initialParsedResponse); // 设置初始值
+
+
+    const handleChange = (value: string) => {
+        setSelectedLanguage(value);
+    };
+
+    // 建立 WebSocket 连接并在接收到消息时更新状态
+    useEffect(() => {
+        const url = `ws://localhost:7070/${selectedLanguage}`;
+        wsRef.current = new WebSocket(url);
+        wsRef.current.onmessage = (e) => {
+            setMessage(e.data);
+            try {
+                const response: Response = JSON.parse(e.data);
+                setParsedResponse(response);
+            } catch (error) {
+                console.error('Failed to parse response:', error);
+            }
+        };
+
+        return () => {
+            wsRef.current?.close();
+        };
+    }, [selectedLanguage]);
+
+    useEffect(() => {
+        let intervalId: NodeJS.Timeout | null = null;
+
+        const sendRequest = () => {
+            if (wsRef.current) {
+                const request: Request = { key: 'ssss' };
+                wsRef.current.send(JSON.stringify(request));
+            }
+        };
+
+        intervalId = setInterval(sendRequest, 1000);
+
+        return () => {
+            clearInterval(intervalId!);
+        };
+    }, [wsRef.current]);
+
+    return (
+        <Layout style={{
+            borderRadius: 8,
+            overflow: 'hidden',
+            width: '100vw',
+            height: '100vh',
+        }}>
+            <Header style={{
+                textAlign: 'left',
+                color: '#000',
+                height: 64,
+                paddingInline: 48,
+                lineHeight: '64px',
+                backgroundColor: backgroundColor,
+                fontSize: 24,
+            }}>
+                AlcOJ Backend Preview
+                <Select
+                    defaultValue="python3"
+                    style={{ width: 120, marginLeft: 20 }}
+                    onChange={handleChange}
+                    options={[
+                        { value: 'python3', label: 'Python3' },
+                        { value: 'golang', label: 'Golang' },
+                    ]}
+                />
+            </Header>
+            <Content style={{
+                textAlign: 'center',
+                lineHeight: '120px',
+                color: '#fff',
+                backgroundColor: '#F0F0F0',
+            }}>
+                <ResponseDisplay {...parsedResponse} />
+            </Content>
+        </Layout>
+    );
+};
+
+export default Backend;
